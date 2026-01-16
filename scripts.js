7608f27d7b790f8a54d2d9790abf3601e672eb8d
Kekovich23 <mr.kekovich@ya.ru>
2026-01-16 17:28:04 +0700

    add modal window and form logic

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-﻿console.log('script check');
+﻿const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const modal = document.querySelector('.modal');
+modal.querySelectorAll('button').forEach(el => el.addEventListener('click', (e) => {
+    modal.style.display = 'none';
+}));
+
+const form = document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let name = form.querySelector('#name');
+    modal.querySelector('#input-name').textContent = name.value;
+
+    let email = form.querySelector('#email').value;
+
+    modal.querySelector('p').style.display = 'none';
+
+    modal.querySelector(emailRegex.test(email) ? '#valid-email' : '#invalid-email').style.display = 'block';
+
+    modal.style.display = 'block';
+});
